3c79d560658c0d86d1f3a0e73fd2f5697aeae852
allmywatts <allmywatts@gmail.com>
2020-04-19 23:25:59 +0200

    Part 1d: Unicafe step 2-5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,18 +3,44 @@ import "./App.css"
 
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <h1>statistics</h1>
-    <p>
-      Good {good}
+const Statistic = ({ text, value }) => {
+  return (
+    <>
+      {text} {value}
       <br></br>
-      Neutral {neutral}
-      <br></br>
-      Bad {bad}
-    </p>
-  </div>
-)
+    </>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={total} />
+        <Statistic text="average" value={average} />
+        <Statistic text="positive" value={positive} />
+      </p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
