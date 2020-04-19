a3e6869d283249d07c7e65ec97748fc84fb2b8ec
allmywatts <allmywatts@gmail.com>
2020-04-19 22:24:50 +0200

    Part 1: Intro - more complex state

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 https://fullstackopen.com/en/about
 
-- Use [prettier](https://www.codereadability.com/automated-code-formatting-with-prettier/) for code formatting
+Use [prettier](https://prettier.io/docs/en/configuration.html) for code formatting.
+Configure as [default formatter](<(https://www.codereadability.com/automated-code-formatting-with-prettier/)>) for vs code.
--- part1/intro/src/index.js
+++ part1/intro/src/index.js
@@ -1,28 +1,83 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
 
-const DateNow = (props) => <p>The time is {props.now.toString()}</p>;
-const Hello = (props) => (
-  <>
-    <p>Hello {props.name}</p>
-  </>
-);
+const DateNow = (props) => <p>The time is {props.now.toString()}</p>
+
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old.
+      </p>
+      <p>You were probably born in {bornYear()}.</p>
+    </div>
+  )
+}
+
+const Display = (props) => {
+  const { left, right } = props.clicks
+  return (
+    <div>
+      <p>Left: {left}</p>
+      <p>Right: {right}</p>
+    </div>
+  )
+}
+
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <div>the app is used by clicking buttons</div>
+  }
+  return <div>All clicks: {allClicks.join(", ")}</div>
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
 
 const App = () => {
+  const [clicks, setClicks] = useState({ left: 0, right: 0 })
+  const [allClicks, setAllClicks] = useState([])
+
+  const handleLeftClick = () => {
+    setAllClicks([...allClicks, "L"])
+    setClicks({
+      ...clicks,
+      left: clicks.left + 1,
+    })
+  }
+
+  const handleRightClick = () => {
+    setAllClicks([...allClicks, "R"])
+    setClicks({
+      ...clicks,
+      right: clicks.right + 1,
+    })
+  }
+
+  const resetClicks = () => {
+    setClicks({ left: 0, right: 0 })
+    setAllClicks([])
+  }
+
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name="Sam" />
-      <Hello name="Satan" />
-      <Hello name="Stan" />
+      <Display clicks={clicks} />
+      <History allClicks={allClicks} />
+      <Button handleClick={handleLeftClick} text="Left" />
+      <Button handleClick={handleRightClick} text="Right" />
+      <Button handleClick={resetClicks} text="Reset" />
+      <Hello name="Satan" age={666} />
       <DateNow now={new Date()} />
     </>
-  );
-};
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
